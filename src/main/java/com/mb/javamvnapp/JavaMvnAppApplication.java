758c80711bc50b16cca79ce7ad489e7fd8c9d94e
moussbed bedril <moussakatbedril@yahoo.com>
2022-01-18 14:46:52 +0400

    Add HelloController

--- src/main/java/com/mb/javamvnapp/JavaMvnAppApplication.java
+++ src/main/java/com/mb/javamvnapp/JavaMvnAppApplication.java
@@ -3,6 +3,8 @@ package com.mb.javamvnapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class JavaMvnAppApplication {
@@ -19,4 +21,15 @@ class Calculation {
     public int add(int i, int i1) {
         return i+i1;
     }
+}
+
+@RestController
+class HelloController{
+
+    @GetMapping("/")
+    public String sayHello(){
+
+        return "Hello world ! ";
+    }
+
 }
